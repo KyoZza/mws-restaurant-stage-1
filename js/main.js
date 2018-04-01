eab3ce53c6f6b8ba5c4529a8b657e4adf73a3a2c
KyoZza <sandro.scheffmann@hotmail.de>
2018-04-01 20:18:58 +0200

    first responsive approach
    feat: first responsive approach
    
    Adapted the page content via css.
    The website follows the mobile first aproach from now on.
    All images and content doesn't overlap anymore.
    A few breakpoints for diverent device sizes are now provided.

--- js/main.js
+++ js/main.js
@@ -143,24 +143,29 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const textContainer = document.createElement('div');
+  textContainer.className = 'restaurants-list-text';
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textContainer.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textContainer.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textContainer.append(address);
+
+  li.append(textContainer);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
-  return li
+  return li;
 }
 
 /**
